053aa41857f02b5ba58d322035fd20f519f8ca6a
Mia <mnestrada@scl.edu.gt>
2025-06-15 07:25:18 -0600

    Ultima version

---
+++ programas/kg.java
@@ -0,0 +1,22 @@
+package programas;
+
+import java.util.Scanner;
+
+public class kg {
+
+     public static void kg() {
+ 
+        Scanner numScann = new Scanner(System.in);
+        System.out.println("+---------------------------------+");
+        System.out.println("[En Kilogramos]: ");
+        double num1 = scannNumeros.nextDouble();
+
+        System.out.println("+---------------------------------+");
+        System.out.println("[Resultado: "+ (num1 * 1000));
+        System.out.println("+---------------------------------+");
+ 
+
+    }
+ 
+
+}
---
+++ programas/macm.java
@@ -0,0 +1,22 @@
+package programas;
+
+import java.util.Scanner;
+ 
+public class macm {
+ 
+     public static void mcm() {
+ 
+        Scanner numScann = new Scanner(System.in);
+        System.out.println("+---------------------------------+");
+        System.out.println("[En metros]: ");
+        double num1 = scannNumeros.nextDouble();
+ 
+        System.out.println("+---------------------------------+");
+        r
+        System.out.println("[Conversion en centimetros]: "+ (num1 * 100));
+ 
+        System.out.println("+---------------------------------+");
+    }
+ 
+
+}
